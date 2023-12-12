ee782528310d5d388afbe2b2f62104ed0aba7923
SAUD421 <saudshahid646@gmail.com>
2023-12-13 01:03:38 +0500

    connected to the database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express= require('express');
 const app= express()
+const mongoose= require('mongoose')
 app.get('/',(req,res)=>
 {
    res.send('NODE API')
 })
+app.get('/blog',(req,res)=>
+{
+   res.send('NODE blog')
+})
 app.listen(3000,()=>
 {
     console.log('NODE API APP IS RUNNING ON PORT 3000')
+})
+mongoose.
+connect('mongodb+srv://admin:12345eddy@cluster0.yrafyko.mongodb.net/Node API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('connected to the database ')
+})
+.catch((error)=>
+{
+    console.log(error)
 })
